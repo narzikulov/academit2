0414510235fd61d9627410c5c1913d5b66c7223e
tim <ntim@bk.ru>
2015-12-08 14:11:43 +0600

    Свой Main для каждого класса

---
+++ Gauss/src/ru/academit/narzikulov/GaussMain.java
@@ -0,0 +1,22 @@
+package ru.academit.narzikulov;
+
+/**
+ * Created by tim on 08.12.2015.
+ */
+public class GaussMain {
+    public static void main(String[] args) {
+        System.out.println();
+        System.out.println("Метода Гаусса:");
+        //double[][] matrixArray16 = {{2, 1, -1, 8}, {-3, -1, 2, -11}, {-2, 1, 2, -3}}; //x = 2, y = 3, z = -1
+        //double[][] matrixArray16 = {{2, -2, 1, -3}, {1, 3, -2, 1}, {3, -1, -1, 2}}; //x = -1.4, y = -2, z = -4.2
+        //double[][] matrixArray16 = {{2, -1, 0, 0}, {-1, 1, 4, 13}, {1, 2, 3, 14}}; //x = 1, y = 2, z = 3
+        double[][] matrixArray16 = {{1, -1, 3, 1, 5}, {4, -1, 5, 4, 4}, {2, -2, 4, 1, 6}, {1, -4, 5, -1, 3}}; //x = 9, y = 18, z = 10, p = -16
+        Gauss matrix16 = new Gauss(matrixArray16);
+        System.out.println("СЛАУ:");
+        matrix16.printAsMatrix();
+        System.out.println("Приведение расширенной матрицу к треугольному виду");
+        matrix16.gauss().printAsMatrix();
+        System.out.println("Решение СЛАУ:");
+        System.out.println(matrix16.linearSystem().toString());
+    }
+}
--- Matrix/src/ru/academit/narzikulov/Main.java
+++ Matrix/src/ru/academit/narzikulov/Main.java
@@ -5,114 +5,8 @@ package ru.academit.narzikulov;
  */
 public class Main {
     public static void main(String[] args) {
-        double[] vectorArray1 = {5, 4, 3, 2, 1};
-        Vector vector1 = new Vector(vectorArray1);
-        System.out.print("Vector1: ");
-        System.out.println(vector1.toString());
-
-        double[] vectorArray2 = {1, 2, 3, 4, 5, 6};
-        Vector vector2 = new Vector(vectorArray2);
-        System.out.print("Vector2: ");
-        System.out.println(vector2.toString());
-
-        System.out.print("Vector1 = Vector1 + Vector2: ");
-        vector1.addVector(vector2);
-        System.out.println(vector1.toString());
-
-        double[] vectorArray11 = {7, 6, 5, 4, 3, 2, 1};
-        Vector vector11 = new Vector(vectorArray11);
-        System.out.print("Vector11: ");
-        System.out.println(vector11.toString());
-
-        vector2.subVector(vector11);
-        System.out.print("Vector2 = Vector2 - Vector11: ");
-        System.out.println(vector2.toString());
-
-        double num = 2.0;
-        vector2.multVectorToNum(num);
-        System.out.printf("Vector2 * %.2f = ", num);
-        System.out.println(vector2.toString());
-
-        vector2.reverseVector();
-        System.out.print("Vector2 * (-1) = ");
-        System.out.println(vector2.toString());
-
-        System.out.print("Vector2 length = ");
-        System.out.println(vector2.getVectorLength());
-
-        int index = 3;
-        System.out.printf("get Vector2 %dd Number = ", index);
-        System.out.println(vector2.getVectorElement(3));
-
-        index = 2;
-        num = 150;
-        vector2.setVectorElement(index, num);
-        System.out.printf("set Vector2 %dd Number = %.2f: ", index, num);
-        System.out.println(vector2.toString());
 
 
-        System.out.println();
-        System.out.println("Vectors comparing");
-        Vector vector3 = new Vector(vectorArray1);
-        Vector vector4 = new Vector(vectorArray1);
-        System.out.print("Vector3 = ");
-        System.out.println(vector3.toString());
-        System.out.print("Vector4 = ");
-        System.out.println(vector4.toString());
-        if (vector3.equals(vector4)) {
-            System.out.println("Vector3 are equal Vector4");
-        } else {
-            System.out.println("Vectors are not equal");
-        }
-        System.out.println("Vector3 hash code = " + vector3.hashCode());
-        System.out.println("Vector4 hash code = " + vector4.hashCode());
-
-        System.out.println("Is Vector3 equal Vector3?");
-        if (vector3.equals(vector3)) {
-            System.out.println("Vectors are equal");
-        } else {
-            System.out.println("Vectors are not equal");
-        }
-
-        System.out.println();
-        System.out.println(vector2.toString());
-        System.out.println(vector4.toString());
-        if (vector2.equals(vector4)) {
-            System.out.println("Vectors are equal");
-        } else {
-            System.out.println("Vectors are not equal");
-        }
-        System.out.println("Vector2 hash code = " + vector2.hashCode());
-        System.out.println("Vector4 hash code = " + vector4.hashCode());
-
-        System.out.println();
-        System.out.println("Vectors addition:");
-        System.out.print("Vector2 = ");
-        System.out.println(vector2.toString());
-
-        System.out.print("Vector4 = ");
-        System.out.println(vector4.toString());
-
-        System.out.print("Vector5 = Vector2 + Vector4 = ");
-        Vector vector5 = Vector.addTwoVectorsToNewOne(vector2, vector4);
-        System.out.println(vector5.toString());
-
-        Vector vector6 = Vector.subVectorsToNewOne(vector2, vector4);
-        System.out.print("Vector6 = Vector2 - Vector4 = ");
-        System.out.println(vector6.toString());
-
-        System.out.print("Vector7 = Vector2 * Vector4 = ");
-        System.out.println(Vector.multVectors(vector2, vector4));
-
-        Vector vector7 = Vector.copyVector(vector6);
-        System.out.print("Vector7 = copy of Vector6 = ");
-        System.out.println(vector7.toString());
-
-        Vector vector8 = new Vector(vector7);
-        System.out.print("Vector8 = copy of Vector7 (constructor) = ");
-        System.out.println(vector8.toString());
-
-        //Matrix
         System.out.println();
         System.out.println("--- Matrix class ---");
         int column = 2;
@@ -130,6 +24,8 @@ public class Main {
         System.out.print("Matrix3: ");
         System.out.println(matrix3.toString());
 
+        double[] vectorArray1 = {5, 4, 3, 2, 1};
+        Vector vector4 = new Vector(vectorArray1);
         System.out.print("Vector4: ");
         System.out.println(vector4.toString());
         double[] vectorArray9 = {3, 3};
@@ -245,18 +141,5 @@ public class Main {
         matrix15.printAsMatrix();
         System.out.println("Определитель = " + matrix15.determinant());
 
-        System.out.println();
-        System.out.println("Метода Гаусса:");
-        //double[][] matrixArray16 = {{2, 1, -1, 8}, {-3, -1, 2, -11}, {-2, 1, 2, -3}}; //x = 2, y = 3, z = -1
-        //double[][] matrixArray16 = {{2, -2, 1, -3}, {1, 3, -2, 1}, {3, -1, -1, 2}}; //x = -1.4, y = -2, z = -4.2
-        //double[][] matrixArray16 = {{2, -1, 0, 0}, {-1, 1, 4, 13}, {1, 2, 3, 14}}; //x = 1, y = 2, z = 3
-        double[][] matrixArray16 = {{1, -1, 3, 1, 5}, {4, -1, 5, 4, 4}, {2, -2, 4, 1, 6}, {1, -4, 5, -1, 3}}; //x = 9, y = 18, z = 10, p = -16
-        Gauss matrix16 = new Gauss(matrixArray16);
-        System.out.println("СЛАУ:");
-        matrix16.printAsMatrix();
-        System.out.println("Приведение расширенной матрицу к треугольному виду");
-        matrix16.gauss().printAsMatrix();
-        System.out.println("Решение СЛАУ:");
-        System.out.println(matrix16.linearSystem().toString());
     }
 }
