fdf8941b476223b0a92329f673cdb9bf4abc3da7
tim <ntim@bk.ru>
2015-12-08 14:23:53 +0600

    Свой Main для каждого класса

---
+++ Main/src/ru/academit/narzikulov/Main.java
@@ -0,0 +1,13 @@
+package ru.academit.narzikulov;
+
+/**
+ * Created by tim on 20.10.2015.
+ */
+public class Main {
+    public static void main(String[] args) {
+        VectorMain.main();
+        MatrixMain.main();
+        GaussMain.main();
+
+    }
+}
