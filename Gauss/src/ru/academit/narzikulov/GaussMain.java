837023b1bf425a50ebce8d658db5a400d938e503
Tim <ntim@bk.ru>
2015-12-09 22:22:32 +0600

    1. Сделан только один public решатель СЛАУ 2. Все public метода заменены на private 3. Класс решателя не должен ничего печатать в консоль. Он должен выдавать некоторый результат, по которому пользователь кода может понять исход, а дальше уже сам напечатать в консоль. Просто в качестве результата решения можно выдавать не вектор, а некоторый объект нового класса, который будет содержать енум для исхода и вектор-результат, если решения есть. 4. Сделайте чтобы основной метод класса Gauss, который решает систему, был нестатическим. Соответственно, ещё часть методов нужно будет сделать нестатическими. При использовании ООП подхода модификатор static встречается очень редко потому что в этом случае нельзя пользоваться наследованием, интерфейсами, перегрузкой методов и т.д. Т.е. почти всем, что дает ООП. Кроме того, если сделать методы не static, то тогда можно хранить матрицу и вектор правой части в полях, что удобнее, чем постоянная передача их между методами.
    P.S. Исправлен механизм использования метода Gauss

--- Gauss/src/ru/academit/narzikulov/GaussMain.java
+++ Gauss/src/ru/academit/narzikulov/GaussMain.java
@@ -20,32 +20,33 @@ public class GaussMain {
         //double[] vectorArray16 = {0, 13, 14};
 
         //x = 9, y = 18, z = 10, p = -16
-        double[][] matrixArray16 = {{1, -1, 3, 1}, {4, -1, 5, 4}, {2, -2, 4, 1}, {1, -4, 5, -1}};
-        double[] vectorArray16 = {5, 4, 6, 3};
+        //double[][] matrixArray16 = {{1, -1, 3, 1}, {4, -1, 5, 4}, {2, -2, 4, 1}, {1, -4, 5, -1}};
+        //double[] vectorArray16 = {5, 4, 6, 3};
 
         //система не имеет решений
         //double[][] matrixArray16 = {{4, -3, 2, -1}, {3, -2, 1, -3}, {5, -3, 1, -8}, {0, 0, 0, 5}};
         //double[] vectorArray16 = {8, 7, 1, 3};
 
         //система имеет множество решений
-        //double[][] matrixArray16 = {{2, 3, -1, 1}, {8, 12, -9, 8}, {4, 6, 3, -2}, {2, 3, 9, -7}};
-        //double[] vectorArray16 = {1, 3, 3, 3};
+        double[][] matrixArray16 = {{2, 3, -1, 1}, {8, 12, -9, 8}, {4, 6, 3, -2}, {2, 3, 9, -7}};
+        double[] vectorArray16 = {1, 3, 3, 3};
 
 
-        Matrix matrix16 = new Matrix (matrixArray16);
-        Vector vector16 = new Vector (vectorArray16);
+        Matrix matrix16 = new Matrix(matrixArray16);
+        Vector vector16 = new Vector(vectorArray16);
         Gauss gausMatrix = new Gauss(matrix16, vector16);
         System.out.println("Коэффициенты уравнений СЛАУ:");
         matrix16.printAsMatrix();
         System.out.println("Вектор: ");
         System.out.println(vector16.toString());
         System.out.println("Решение СЛАУ:");
-        if (gausMatrix.linearSystem() == null) {
+        Vector linearSystemSolve = gausMatrix.linearSystem();
+        if (linearSystemSolve == null) {
             System.out.println("Система не имеет решений");
-        } else if (gausMatrix.linearSystem().getSize() == 0) {
+        } else if (linearSystemSolve.getSize() == 0) {
             System.out.println("Система имеет множество решений");
         } else {
-            System.out.println(gausMatrix.linearSystem().toString());
+            System.out.println(linearSystemSolve.toString());
         }
     }
 
