0ca7047472758ad23d2762e73c30482d60bf80bb
tim <ntim@bk.ru>
2015-12-16 17:49:09 +0600

    interface Shape

--- Main/src/ru/academit/narzikulov/Main.java
+++ Main/src/ru/academit/narzikulov/Main.java
@@ -1,5 +1,7 @@
 package ru.academit.narzikulov;
 
+import java.awt.*;
+
 /**
  * Created by tim on 20.10.2015.
  */
@@ -7,6 +9,7 @@ public class Main {
     public static void main(String[] args) {
         //VectorMain.main();
         //MatrixMain.main();
-        GaussMain.main();
+        //GaussMain.main();
+        ShapeMain.main();
     }
 }
