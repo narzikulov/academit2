a5b2cb1d5d9e48bf6d0e899be5fd0bef7b16d716
tim <ntim@bk.ru>
2016-04-24 17:01:13 +0600

    Класс Point практ занятие лекция 1 ООП

--- Main/src/ru/academit/narzikulov/Main.java
+++ Main/src/ru/academit/narzikulov/Main.java
@@ -11,6 +11,13 @@ public class Main {
         //MatrixMain.main();
         //GaussMain.main();
         //ShapeMain.main();
-        HashTableMain.main();
+        //HashTableMain.main();
+
+        Point p = new Point(0, 0);
+        Point p1 = new Point(3, 4);
+        p.printCoordinates();
+        p1.printCoordinates();
+        System.out.println();
+        System.out.println("Dist:" + p.dist(p1));
     }
 }
