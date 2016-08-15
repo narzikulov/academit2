82015469e3ef5eb0758e97a0ea1bd8a51b4b47b8
tim <ntim@bk.ru>
2016-08-15 17:59:00 +0700

    Квадратное уравнение JavaScript

--- JavaSripts/src/quadratic_equation.js
+++ JavaSripts/src/quadratic_equation.js
@@ -1,19 +1,21 @@
 function solveEquation(){
-    var aKoeff = document.getElementById("a-koeff").value;
-    var bKoeff = document.getElementById("b-koeff").value;
-    var cKoeff = document.getElementById("c-koeff").value;
+    var aKoeff = parseInt(document.getElementById("a-koeff").value);
+    var bKoeff = parseInt(document.getElementById("b-koeff").value);
+    var cKoeff = parseInt(document.getElementById("c-koeff").value);
 
-    var discrimenant = Math.sqrt(bKoeff * bKoeff - 4 * aKoeff * cKoeff);
+    var discrimenant = bKoeff * bKoeff - 4 * aKoeff * cKoeff;
     var result="There is no solve for this equation";
 
     if (discrimenant > 0 ) {
-        var x1 = (bKoeff + discrimenant) / (2 * aKoeff);
-        var x2 = (-1 * bKoeff + discrimenant) / (2 * aKoeff);
-        result = "x1 = ";
+        var x1 = (-1 * bKoeff + Math.sqrt(discrimenant)) / (2 * aKoeff);
+        var x2 = (-1 * bKoeff - Math.sqrt(discrimenant)) / (2 * aKoeff);
+        result = ("x1 = " + x1 + "; x2 = " + x2);
     }
-
+    if (discrimenant === 0 ) {
+        var x1 = (-1 * bKoeff / (2 * aKoeff));
+        result = ("The only solve for this equation is x1 = " + x1);
+    }
     alert(result);
 }
-
 
 document.getElementById("solve").onclick = solveEquation;
