244a07873b4e57dc589dc636d75e01ab0bbb3371
tim <ntim@bk.ru>
2016-08-16 11:05:57 +0700

    Квадратное уравнение JavaScript

--- JavaSripts/src/quadratic_equation.js
+++ JavaSripts/src/quadratic_equation.js
@@ -1,18 +1,19 @@
 function solveEquation(){
-    var aKoeff = parseInt(document.getElementById("a-koeff").value);
-    var bKoeff = parseInt(document.getElementById("b-koeff").value);
-    var cKoeff = parseInt(document.getElementById("c-koeff").value);
+    var aCoeff = parseInt(document.getElementById("a-coeff").value);
+    var bCoeff = parseInt(document.getElementById("b-coeff").value);
+    var cCoeff = parseInt(document.getElementById("c-coeff").value);
 
-    var discrimenant = bKoeff * bKoeff - 4 * aKoeff * cKoeff;
+    var discriminant = bCoeff * bCoeff - 4 * aCoeff * cCoeff;
     var result="There is no solve for this equation";
+    var x1;
+    var x2;
 
-    if (discrimenant > 0 ) {
-        var x1 = (-1 * bKoeff + Math.sqrt(discrimenant)) / (2 * aKoeff);
-        var x2 = (-1 * bKoeff - Math.sqrt(discrimenant)) / (2 * aKoeff);
+    if (discriminant > 0 ) {
+        x1 = (-1 * bCoeff + Math.sqrt(discriminant)) / (2 * aCoeff);
+        x2 = (-1 * bCoeff - Math.sqrt(discriminant)) / (2 * aCoeff);
         result = ("x1 = " + x1 + "; x2 = " + x2);
-    }
-    if (discrimenant === 0 ) {
-        var x1 = (-1 * bKoeff / (2 * aKoeff));
+    } else if (discriminant === 0 ) {
+        x1 = (-1 * bCoeff / (2 * aCoeff));
         result = ("The only solve for this equation is x1 = " + x1);
     }
     alert(result);
