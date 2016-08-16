5d861f93adc2a8c60261dfff954da71e6343b116
tim <ntim@bk.ru>
2016-08-16 11:10:52 +0700

    Квадратное уравнение JavaScript

--- JavaSripts/src/quadratic_equation.js
+++ JavaSripts/src/quadratic_equation.js
@@ -9,11 +9,11 @@ function solveEquation(){
     var x2;
 
     if (discriminant > 0 ) {
-        x1 = (-1 * bCoeff + Math.sqrt(discriminant)) / (2 * aCoeff);
-        x2 = (-1 * bCoeff - Math.sqrt(discriminant)) / (2 * aCoeff);
+        x1 = (-bCoeff + Math.sqrt(discriminant)) / (2 * aCoeff);
+        x2 = (-bCoeff - Math.sqrt(discriminant)) / (2 * aCoeff);
         result = ("x1 = " + x1 + "; x2 = " + x2);
     } else if (discriminant === 0 ) {
-        x1 = (-1 * bCoeff / (2 * aCoeff));
+        x1 = (-bCoeff / (2 * aCoeff));
         result = ("The only solve for this equation is x1 = " + x1);
     }
     alert(result);
