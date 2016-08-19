a6fa9756437e0ca55a6606f5a5b87f8792d52a7c
tim <ntim@bk.ru>
2016-08-19 17:34:06 +0700

    Списки JavaScript

---
+++ JavaSripts/src/JavaScriptLists/script.js
@@ -0,0 +1,50 @@
+/*
+ Создайте список чисел
+ • Отсортируйте его по убыванию
+ • Получите подсписок из первых 5 элементов и подсписок
+ из последних 5 элементов
+ • Найдите сумму элементов списка, которые являются
+ четными числами
+ • Создайте список чисел от 1 до 100, в таком порядке
+ */
+
+var list = [5, 23, 6, 1, -45, 0, 31, 7, 10, -6, 2, 12, 3];
+
+function printList(printingList) {
+    var str = "{";
+    for (var i = 0; i < printingList.length - 1; ++i) {
+        str += printingList[i] + "; ";
+    }
+    str += printingList[printingList.length - 1] + "}";
+    return str;
+}
+console.log("Unsorted list: " + printList(list));
+
+list.sort(function (num1, num2) {
+    return num2 - num1;
+});
+console.log("Sorted list: " + printList(list));
+
+var firstFiveElementsList = list.slice(0, 5);
+console.log("First five elements: " + printList(firstFiveElementsList));
+
+var lastFiveElementsList = list.slice(list.length - 5)
+console.log("Last five elements: " + printList(lastFiveElementsList));
+
+function sumEvenNums(fullList) {
+    var result = 0;
+    for (var i = 0; i < fullList.length; ++i) {
+        if (fullList[i] % 2 === 0) {
+            result += fullList[i];
+        }
+    }
+    return result;
+}
+
+console.log("Sum even numbers of list: " + sumEvenNums(list));
+
+var firstHundredList = [];
+for (var i = 0; i < 100; ++i) {
+    firstHundredList[i] = i + 1;
+}
+console.log("First hundred list: " + printList(firstHundredList));
