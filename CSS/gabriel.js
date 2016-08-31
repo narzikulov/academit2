aa93775c2df1d005bae941c85b5b204a5e58dfe1
tim <ntim@bk.ru>
2016-08-31 18:28:58 +0700

    Gabriel CSS
    1. Не подключен css reset.
    Это всегда нужно делать, чтобы во всех браузерах было одинаково
    
    2. При наведении на кнопки курсор должен превращаться в "руку". Чтобы пользователь понимал, что по клику произойдёт какое-то действие.
    
    3. Нет стилей для заголовка текущей выбранной вкладки (Gabriel)
    
    4. Сделайте, чтобы в таблицах было побольше строк, чтобы появился скролл в них.
    Скролл пока можно сделать обычный браузерный, не кастомный как на скриншоте
    
    5. Размеры шрифтов, цвета и отступы должны быть максимально близки к макету.
    Сейчас довольно много расхождений.
    
    6. Не соблюдены отступы в самой нижней таблице, должно быть так:
    http://joxi.ru/4AkOwPeiBV4bAq
    В правой части формы тоже есть расхождения.
    Всё должно быть выровнено так, как на макете.
    
    7. Если присмотреться, то в самой нижней таблицы границы двухцветные, которые точками
    
    8. #table - плохой id. Прям наверняка встретится где-то ещё
    Аналогично #buttons, #menu
    
    9. Селекторам по id следует предпочитать селекторы по классу.
    Тут нет ограничения на уникальность.
    Но это можно не поправлять, так пойдёт.
    
    10. Ширину полей ввода demand-ET не стоит делать в процентах.
    При уменьшении размеров экрана они становятся слишком мелкими.
    Там лучше сделать фиксированную ширину.
    
    11. В проекте картинки лучше запихнуть в отдельную папку типа img или images

---
+++ CSS/gabriel.js
@@ -0,0 +1,26 @@
+/**
+ * Created by tim on 31.08.2016.
+ */
+$(document).ready(function () {
+    var value = 0;
+
+    $("#etslider").slider({
+        value: 200,
+        min: 0,
+        max: 1000,
+        step: 1,
+        create: function (event, ui) {
+            value = $("#etslider").slider("value");
+            $("#etSliderVal").val(ui.val);//Заполняем этим значением элемент с id
+        },
+        slide: function (event, ui) {
+            $("#etSliderVal").val(ui.value);//При изменении значения ползунка заполняем элемент с id contentSlider
+        }
+    });
+
+    value = $("#etslider").slider("value");
+    $("#etSliderVal").val(value);
+
+
+});
+
