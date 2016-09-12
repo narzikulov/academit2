af52cde6c19dfef432888da69ebf7fb8c317b15f
tim <ntim@bk.ru>
2016-09-12 15:15:14 +0700

    Phonebook

--- Phonebook/js/phonebook.js
+++ Phonebook/js/phonebook.js
@@ -2,35 +2,35 @@
  * Created by tim on 31.08.2016.
  */
 $(document).ready(function () {
-    $("html").niceScroll({cursorcolor:"#a19f98", autohidemode: true});
-    $("div #phoneBookTable").niceScroll({cursorcolor:"#a19f98", autohidemode: false, cursorwidth: '10px'});
+    $("html").niceScroll({cursorcolor: "#a19f98", autohidemode: true});
+    $("div #phoneBookTable").niceScroll({cursorcolor: "#a19f98", autohidemode: false, cursorwidth: '10px'});
 
     var index = 1;
 
-    var reorderRows = function() {
+    var reorderRows = function () {
         var rows = $("#phoneBookTable table").find("tbody").find("tr");
         index = rows.length + 1;
 
-        $(rows).each(function(i) {
+        $(rows).each(function (i) {
             $(this).find("td").eq(0).text(i + 1);
         });
     };
 
-     var reFillTable = function() {
-        $("#phoneBookTable table").find("tbody").find("tr").each(function(i) {
+    var reFillTable = function () {
+        $("#phoneBookTable table").find("tbody").find("tr").each(function (i) {
             if ((i + 1) % 2 === 0) {
                 this.setAttribute("class", "filled");
             } else {
                 this.setAttribute("class", "unfilled");
             }
         });
-     };
+    };
 
-    $(".inputFormTable .inputTD").focus(function() {
+    $(".inputFormTable .inputTD").focus(function () {
         $(this).select();
     });
 
-    $("#saveToPhoneBookButton").click(function (){
+    $("#saveToPhoneBookButton").click(function () {
         var firstName = $("#firstName").val();
         var lastName = $("#lastName").val();
         var middleName = $("#middleName").val();
@@ -45,45 +45,83 @@ $(document).ready(function () {
         var commentsTDTag = "<td class='comments'>" + comments + "</td>";
         var delRecTDTag = "<td class='deleteRecord'><img src='img/basket.png'><input type='checkbox' class='deleteRecordCheckBox'></td>";
 
+        $("#phoneBookTable .deleteRecordCheckBox").click(
+            function () {
+                if ($(this).prop('checked')) {
+                    $(this).attr("checked", "checked");
+                } else {
+                    $(this).removeAttr("checked");
+                }
+            }
+        );
+
         var filledTRTag = "<tr>";
         if (index % 2 === 0) {
             filledTRTag = "<tr class='filled'>";
         }
 
+        var attention = $(".inputFormTable .titleTD span");
         if ((firstName != "" || lastName != "") && phoneNumber != "") {
             $("#phoneBookTable tbody").append(filledTRTag + indexTDTag + firstNameTDTag + secondNameTDTag + middleNameTDTag + phoneNumberTDTag + commentsTDTag + delRecTDTag + "</tr>");
             ++index;
-            clearForm();
-            $("div #errorMessage").text("");
-            $("div #errorMessage").text("");
-            $(".inputFormTable .attention").css("color", "black");
+            //clearForm();
+            $("#errorMessage").text("");
+            attention.attr("class", "");
         } else {
             $("div #errorMessage").text("Не заполнены обязательные поля, помеченные звездочкой!");
-            $(".inputFormTable .attention").css("color", "red");
+            attention.attr("class", "attention");
         }
 
         $(".deleteRecord img").click(function () {
             $("div #errorMessage").text("");
-            $(".inputFormTable .attention").css("color", "black");
+            attention.attr("class", "");
             $(this).closest("tr").remove();
             reorderRows();
             reFillTable();
         });
-
     });
 
+    $("#phoneBookTableFixedTitle .deleteRecordCheckBox").click(
+        function () {
+            var allCheckboxes = $("#phoneBookTable").find(".deleteRecordCheckBox");
+            if ($(this).prop('checked')) {
+                allCheckboxes.each(function () {
+                    this.checked = true;
+                });
+            } else {
+                allCheckboxes.each(function () {
+                    this.checked = false;
+                });
+            }
+        }
+    );
 
-    var clearForm = function (){
-                var firstName = $("#firstName").val("");
-                var lastName = $("#lastName").val("");
-                var middleName = $("#middleName").val("");
-                var phoneNumber = $("#phoneNumber").val("");
-                var comments = $("#comments").val("");
-                $("div #errorMessage").text("");
-                $(".inputFormTable .attention").css("color", "black");
+    $("#delChecked").click(function () {
+        var allCheckboxes = $("#phoneBookTable").find(".deleteRecordCheckBox");
+        var allCheckedCheckboxes = allCheckboxes.filter(
+            function () {
+                return $(this).prop("checked");
+            }
+        );
+        $(allCheckedCheckboxes).closest("tr").remove();
+        reorderRows();
+        reFillTable();
+        document.getElementById('delAllCheckbox').checked = false;
+    });
+
+    function clearForm() {
+        $("#firstName").val("");
+        $("#lastName").val("");
+        $("#middleName").val("");
+        $("#phoneNumber").val("");
+        $("#comments").val("");
+        $("div #errorMessage").text("");
+        var attention = $(".inputFormTable .titleTD span");
+        attention.attr("class", "");
     };
 
-    $("#clearForm").click(clearForm);
+    $("#clearForm").click(function () {
+        clearForm();
+    });
 
 });
-
