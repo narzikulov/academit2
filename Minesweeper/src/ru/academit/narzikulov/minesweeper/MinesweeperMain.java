f388fabfa41a1c3eefa8f93e4b143668ccce8448
tim <ntim@bk.ru>
2016-12-01 15:21:37 +0700

    Phonebook

--- Minesweeper/src/ru/academit/narzikulov/minesweeper/MinesweeperMain.java
+++ Minesweeper/src/ru/academit/narzikulov/minesweeper/MinesweeperMain.java
@@ -1,21 +1,23 @@
 package ru.academit.narzikulov.minesweeper;
 
-import ru.academit.narzikulov.minesweeper.text.MinesweeperText;
+import ru.academit.narzikulov.minesweeper.gui.MinesweeperGui;
+
+import javax.swing.*;
 
 /**
  * Created by tim on 18.05.2016.
  */
 public class MinesweeperMain {
     public static void main() {
-        /*SwingUtilities.invokeLater(new Runnable() {
-            /public void run() {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
                 MinesweeperGui minesweeper = new MinesweeperGui();
             }
-        });*/
+        });
 
-        MinesweeperText minesweeperText = new MinesweeperText();
-        minesweeperText.printMineField();
-        minesweeperText.startGame();
+        //MinesweeperText minesweeperText = new MinesweeperText();
+        //minesweeperText.printMineField();
+        //minesweeperText.startGame();
 
     }
 }
